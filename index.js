9a178586b104aa679b184dc2a5b8accaef5a6a03
Nickolay <snsarkisyan@yandex.ru>
2024-05-13 08:25:19 +0300

    add/exceptions,middleware

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const apiRoutes = require("./src/router/hospital-routes.js");
 const { DB_CONNECTION, PORT } = require("./config.js");
-
+const { errorMiddleware } = require("./src/middleware/error-middleware.js");
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 app.use("/", apiRoutes);
+app.use(errorMiddleware);
+
 const loadApp = async () => {
   try {
     await mongoose.connect(DB_CONNECTION);
---
+++ src/middleware/error-middleware.js
@@ -0,0 +1,14 @@
+const { validationResult } = require("express-validator");
+const ApiError = require("../exceptions/api-error.js");
+
+const errorMiddleware = (err, req, res, next) => {
+  console.log(err);
+  if (err instanceof ApiError) {
+    return res
+      .status(err.status)
+      .json({ message: err.message, errors: err.errors });
+  }
+  return res.status(500).json({ message: "Непредвиденная ошибка" });
+};
+
+module.exports = { errorMiddleware };
